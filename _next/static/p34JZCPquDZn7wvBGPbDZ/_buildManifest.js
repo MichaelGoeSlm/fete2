9acc425ccabd300823c2aeaa85064d3038844b45
Michael <michael@devhub.wiki>
2026-02-01 05:02:50 +0000

    Fix ChatModal - remove motion.div, add initialized state

---
+++ _next/static/p34JZCPquDZn7wvBGPbDZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/207-45d471a3227f8d1c.js","static/chunks/pages/index-f49b70f2b3704742.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
